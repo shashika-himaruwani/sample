b6631643cfc678b8e9a217c2396bb1352712440d
shashika-himaruwani <sendhimaruwani@gmail.com>
2024-06-14 17:54:17 +0530

    Delete practice directory

--- practice/script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Thank you for your message! We will get back to you soon.');
-    this.reset();
-});
